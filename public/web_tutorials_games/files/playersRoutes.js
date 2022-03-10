ac5ff7cb5538d81b9a5176940441d8b1b2fa6b2c
Miguel Bugalho <miguel.bugalho@gmail.com>
2022-03-10 23:19:08 +0000

    Part of the authentication tutorial Still needs to be finished

---
+++ public/web_tutorials_games/files/playersRoutes.js
@@ -0,0 +1,31 @@
+
+var express = require('express');
+var router = express.Router();
+var uModel = require("../models/playersModel");
+var auth = require("../utils/authentication")
+            
+router.post('/login', async function(req, res, next) {
+    console.log("Login")
+    let name = req.body.name;
+    let password = req.body.password;
+    let result = await uModel.loginCheck(name,password);
+    if (result.status == 200) {
+        auth.saveUserId(res,result.result.userId);
+        res.status(result.status).send({msg:"User logged in"});
+    } else  res.status(result.status).send(result.result);
+});
+
+router.post('/logout', auth.checkAuthentication, async function(req, res, next) {
+    console.log("Logout")
+    auth.logout(res);    
+    res.status(200).send({msg:"User logged out"});
+});
+
+router.get('/profile', auth.checkAuthentication, async function(req, res, next) {
+    console.log("Get profile of logged user ");
+    let result = await uModel.getLoggedUserInfo(req.userId);
+    res.status(result.status).send(result.result);
+});
+
+
+module.exports = router;
